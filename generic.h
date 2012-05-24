14c6776c08fded31adaa9f4fbc268d3f25f77f9d
Sebastian Fedrau <lord-kefir@arcor.de>
2012-05-24 07:46:16 +0200

    fixed 'invalid' include directive

--- generic.h
+++ generic.h
@@ -2,10 +2,10 @@
 #define __GENERIC_H__
 
 #include <stdbool.h>
-
-#include "stdint.h"
+#include <stdint.h>
 
 typedef int32_t (*CompareFunc)(const void *a, const void *b);
+typedef bool (*EqualFunc)(const void *a, const void *b);
 typedef void (*FreeFunc)(void *p);
 typedef bool (*ForeachKeyValuePairFunc)(void *key, void *value, void *user_data);
 #define str_compare (CompareFunc)strcmp
