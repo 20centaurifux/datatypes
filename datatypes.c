bcb854b45cab1d89ee3ded0c158287d320b6068c
Sebastian Fedrau <lord-kefir@arcor.de>
2012-06-04 21:44:53 +0200

    added default int & str compare functions

---
+++ datatypes.c
@@ -0,0 +1,31 @@
+#include "datatypes.h"
+
+bool
+str_equal(const void * restrict a, const void * restrict b)
+{
+	const char *s0 = (const char *)a;
+	const char *s1 = (const char *)b;
+
+	while(*s0 && *s1)
+	{
+		if(*s0++ != *s1++)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int32_t
+int_compare(const void *a, const void *b)
+{
+	return (int)a - (int)b;
+}
+
+inline bool
+int_equal(const void * restrict a, const void * restrict b)
+{
+	return (int *)a == (int *)b;
+}
+
--- datatypes.h
+++ datatypes.h
@@ -7,7 +7,12 @@
 typedef int32_t (*CompareFunc)(const void *a, const void *b);
 typedef bool (*EqualFunc)(const void *a, const void *b);
 typedef void (*FreeFunc)(void *p);
+
 #define str_compare (CompareFunc)strcmp
+int32_t int_compare(const void *a, const void *b);
+
+bool str_equal(const void * restrict a, const void * restrict b);
+bool int_equal(const void * restrict a, const void * restrict b);
 
 typedef struct _Allocator
 {
