08df5221f99555817654b008cee53e7852071e89
lordkefir <lord-kefir@arcor.de>
2012-06-29 23:59:18 +0200

    stack implementation

---
+++ stack.c
@@ -0,0 +1,46 @@
+/***************************************************************************
+    begin........: June 2012
+    copyright....: Sebastian Fedrau
+    email........: lord-kefir@arcor.de
+ ***************************************************************************/
+
+/***************************************************************************
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+ ***************************************************************************/
+/*!
+ * \file stack.c
+ * \brief A generic stack.
+ * \author Sebastian Fedrau <lord-kefir@arcor.de>
+ * \version 0.1.0
+ * \date 29. June 2012
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include "stack.h"
+
+bool
+stack_pop(Stack *stack, void **data)
+{
+	assert(stack != NULL);
+	assert(data != NULL);
+
+	if(slist_count(stack))
+	{
+		*data = slist_pop(stack);
+
+		return true;
+	}
+
+	*data = NULL;
+
+	return false;
+}
---
+++ stack.h
@@ -0,0 +1,109 @@
+/***************************************************************************
+    begin........: June 2012
+    copyright....: Sebastian Fedrau
+    email........: lord-kefir@arcor.de
+ ***************************************************************************/
+
+/***************************************************************************
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+ ***************************************************************************/
+/*!
+ * \file stack.h
+ * \brief A generic stack.
+ * \author Sebastian Fedrau <lord-kefir@arcor.de>
+ * \version 0.1.0
+ * \date 29. June 2012
+ */
+
+#ifndef __STACK_H__
+#define __STACK_H__
+
+#include "slist.h"
+
+/**
+ *\struct Stack
+ *\brief A generic stack.
+ */
+typedef SList Stack;
+
+/**
+ *\struct StackItem
+ *\brief Holds stack item data & pointer to next element.
+ */
+typedef SList StackItem;
+
+/**
+ *\param equals function to compare item data
+ *\param free function to free item data or NULL
+ *\param allocator a user-defined memory allocator for creating/destroying StackItems or NULL
+ *\return a new Stack
+ *
+ * Creates a new Stack.
+ */
+#define stack_new(equals, free, allocator) slist_new(equals, free, allocator)
+
+/**
+ *\param stack a Stack
+ *\param equals function to compare item data
+ *\param free function to free item data or NULL
+ *\param allocator a user-defined memory allocator for creating/destroying StackItems or NULL
+ *
+ * Initializes a Stack.
+ */
+#define stack_init(stack, equals, free, allocator) slist_init(stack, equals, free, allocator)
+
+/**
+ *\param stack a Stack
+ *
+ * Destroys all items in the stack. Frees also memory allocated for the Stack instance.
+ */
+#define stack_destroy(stack) slist_destroy(stack)
+
+/**
+ *\param stack a Stack
+ *
+ * Destroys all items in the stack.
+ */
+#define stack_free(stack) slist_free(stack)
+
+/**
+ *\param stack a Stack
+ *\param data data to push
+ *
+ * Pushs data onto the stack.
+ */
+#define stack_push(stack, data) slist_prepend(stack, data)
+
+/**
+ *\param stack a Stack
+ *\param data location to store data
+ *\return true if stack is not empty
+ *
+ * Removes first element from stack.
+ */
+bool stack_pop(Stack *stack, void **data);
+
+/**
+ *\param stack a Stack
+ *
+ * Clears a stack.
+ */
+#define stack_clear(stack) slist_clear(stack)
+
+/**
+ *\param stack a Stack
+ *\return number of items
+ *
+ * Gets the number of items.
+ */
+#define stack_count(stack) slist_count(stack)
+
+#endif
+
