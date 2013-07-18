ea43945cb1c69513694e5aa0bbec4eb124104514
Sebastian Fedrau <lord-kefir@arcor.de>
2013-07-18 17:50:17 +0200

    changed license to GPLv3

--- stack.h
+++ stack.h
@@ -6,13 +6,13 @@
 
 /***************************************************************************
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU General Public License v3 as published by
     the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-    General Public License for more details.
+    General Public License v3 for more details.
  ***************************************************************************/
 /*!
  * \file stack.h
